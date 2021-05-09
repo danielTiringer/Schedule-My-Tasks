5d88d1d796b0653d210a0666b5489e46a79bd572
Daniel Tiringer <tiringerdaniel@tutanota.com>
2021-05-09 08:18:49 +0200

    Move container to header-footer

--- src/app/Views/users/register.php
+++ src/app/Views/users/register.php
@@ -1,58 +1,56 @@
-<div class="container">
-    <div class="row">
-        <div class="col-12 col-sm-8 offset-sm-2 col-md-6 offset-md-3 mt-5 pt-3 pb-3 bg-white form-wrapper">
-            <div class="container">
-                <h3>Register</h3>
-                <hr>
-                <form class="" action="/register" method="POST">
-                    <div class="row">
-                        <div class="col-12 col-sm-6">
-                            <div class="form-group">
-                                <label for="first_name">First Name</label>
-                                <input type="text" class="form-control" name="first_name" id="first_name" value="<?= set_value('first_name') ?>">
-                            </div>
+<div class="row">
+    <div class="col-12 col-sm-8 offset-sm-2 col-md-6 offset-md-3 mt-5 pt-3 pb-3 bg-white form-wrapper">
+        <div class="container">
+            <h3>Register</h3>
+            <hr>
+            <form class="" action="/register" method="POST">
+                <div class="row">
+                    <div class="col-12 col-sm-6">
+                        <div class="form-group">
+                            <label for="first_name">First Name</label>
+                            <input type="text" class="form-control" name="first_name" id="first_name" value="<?= set_value('first_name') ?>">
                         </div>
-                        <div class="col-12 col-sm-6">
-                            <div class="form-group">
-                                <label for="last_name">Last Name</label>
-                                <input type="text" class="form-control" name="last_name" id="last_name" value="<?= set_value('last_name') ?>">
-                            </div>
+                    </div>
+                    <div class="col-12 col-sm-6">
+                        <div class="form-group">
+                            <label for="last_name">Last Name</label>
+                            <input type="text" class="form-control" name="last_name" id="last_name" value="<?= set_value('last_name') ?>">
                         </div>
+                    </div>
+                    <div class="col-12">
+                        <div class="form-group">
+                            <label for="email">Email address</label>
+                            <input type="text" class="form-control" name="email" id="email" value="<?= set_value('email') ?>">
+                        </div>
+                    </div>
+                    <div class="col-12 col-sm-6">
+                        <div class="form-group">
+                            <label for="password">Password</label>
+                            <input type="password" class="form-control" name="password" id="password" value="">
+                        </div>
+                    </div>
+                    <div class="col-12 col-sm-6">
+                        <div class="form-group">
+                            <label for="password_confirm">Confirm Password</label>
+                            <input type="password" class="form-control" name="password_confirm" id="password_confirm" value="">
+                        </div>
+                    </div>
+                    <? if (isset($validation)): ?>
                         <div class="col-12">
-                            <div class="form-group">
-                                <label for="email">Email address</label>
-                                <input type="text" class="form-control" name="email" id="email" value="<?= set_value('email') ?>">
-                            </div>
+                            <div class="alert alert-danger" role="alert">
+                            <?= $validation->listErrors() ?>
                         </div>
-                        <div class="col-12 col-sm-6">
-                            <div class="form-group">
-                                <label for="password">Password</label>
-                                <input type="password" class="form-control" name="password" id="password" value="">
-                            </div>
-                        </div>
-                        <div class="col-12 col-sm-6">
-                            <div class="form-group">
-                                <label for="password_confirm">Confirm Password</label>
-                                <input type="password" class="form-control" name="password_confirm" id="password_confirm" value="">
-                            </div>
-                        </div>
-                        <? if (isset($validation)): ?>
-                            <div class="col-12">
-                                <div class="alert alert-danger" role="alert">
-                                <?= $validation->listErrors() ?>
-                            </div>
-                        <? endif; ?>
+                    <? endif; ?>
+                </div>
+                <div class="row">
+                    <div class="col-12 col-sm-4">
+                        <button type="submit" class="btn btn-primary">Register</button>
                     </div>
-                    <div class="row">
-                        <div class="col-12 col-sm-4">
-                            <button type="submit" class="btn btn-primary">Register</button>
-                        </div>
-                        <div class="col-12 col-sm-8 text-right">
-                            <a href="/login">Already registered?</a>
-                        </div>
+                    <div class="col-12 col-sm-8 text-right">
+                        <a href="/login">Already registered?</a>
                     </div>
-                </form>
-            </div>
+                </div>
+            </form>
         </div>
     </div>
 </div>
