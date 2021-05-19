bb81880eb742d20d2aa2df2b5bfd375607f77bff
Daniel Tiringer <tiringerdaniel@tutanota.com>
2021-05-19 19:08:07 +0200

    Move dailytask generation from controller to model

--- src/app/Models/DailyTasks.php
+++ src/app/Models/DailyTasks.php
@@ -3,6 +3,9 @@
 namespace App\Models;
 
 use CodeIgniter\Model;
+use Config\Database;
+use DateTime;
+use DateTimeZone;
 
 class DailyTasks extends Model
 {
@@ -42,4 +45,49 @@ class DailyTasks extends Model
 	protected $afterFind            = [];
 	protected $beforeDelete         = [];
 	protected $afterDelete          = [];
+
+	public function generateDailyTasks()
+	{
+		$db = Database::connect();
+		$builder = $db->table('todos')
+					->select('id')
+					->where('status', 'active');
+
+		$intervals = ['daily'];
+
+		if ($this->isItMonday()) {
+			$intervals[] = 'weekly';
+		}
+
+		if ($this->isItFirstOfTheMonth()) {
+			$intervals[] = 'monthly';
+		}
+
+		$query = $builder->whereIn('interval', $intervals)->get();
+
+		foreach ($query->getResult() as $result) {
+			$data = ['todos_id' => $result->id];
+			$this->insert($data);
+		}
+	}
+
+	private function isItMonday(): bool
+	{
+		$currentTime = $this->getCurrentTime();
+
+		return $currentTime->format('l') === 'Monday';
+	}
+
+	private function isItFirstOfTheMonth(): bool
+	{
+		$currentTime = $this->getCurrentTime();
+		return $currentTime->format('d') === '1';
+	}
+
+	private function getCurrentTime(): DateTime
+	{
+		$timeZone = new DateTimeZone('Europe/Budapest');
+		return new DateTime('now', $timeZone);
+
+	}
 }
